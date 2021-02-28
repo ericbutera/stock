da94da561b868054c9df1c784a9ae20f1f79c401
Eric Butera <eric@ericbutera.com>
2021-02-28 15:17:04 -0500

    Add bootstrap 5

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+// bootstrap5
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
